a0d501f36363d1be00e9dd61a9cfd19e0acc593a
affanthegreat <32616893+affanthegreat@users.noreply.github.com>
2022-02-02 15:40:32 +0530

    part 2 complete

---
+++ part3/datamock/server.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"log"
+	"net"
+	p "part3/proto"
+	"strconv"
+
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	p.UnimplementedAddServiceServer
+}
+
+func main() {
+	listener, err := net.Listen("tcp", ":4041")
+	if err != nil {
+		panic(err)
+	}
+	srv := grpc.NewServer()
+
+	p.RegisterAddServiceServer(srv, &server{})
+	log.Printf("server listening at %v", listener.Addr())
+	if err := srv.Serve(listener); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+
+}
+func (s *server) GetMockUserData(ctx context.Context, name *p.SearchQuery) (*p.User, error) {
+	a := name.Query
+	b := len([]rune(a))
+	c, err := strconv.ParseInt(strconv.Itoa(b*10), 10, 64)
+	if b < 6 {
+		return nil, errors.New("Length of Name smaller than 6 characters")
+	} else if err == nil {
+		return &p.User{Name: a, Class: strconv.Itoa(b), Roll: c}, nil
+	}
+	return nil, err
+}
